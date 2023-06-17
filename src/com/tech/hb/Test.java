88e3db127cee96162ab815fd29e27f8d1f280e98
HindawiInfoTech <fusioninstitutetech@gmail.com>
2023-06-17 12:00:18 +0530

    initial commit

--- src/com/tech/hb/Test.java
+++ src/com/tech/hb/Test.java
@@ -8,7 +8,7 @@ public class Test {
 
 	public static void main(String[] args) {
 	
-		System.out.println("111");
+		System.out.println("1111");
 		Configuration cfg = new  Configuration();
 		System.out.println("222");
 		cfg.configure("hibernate-cfg.xml");
